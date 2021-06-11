7aaffe860db877e5e131390fe204063b19204d44
Brousse <mbweb47@gmail.com>
2021-06-11 14:06:45 +0200

    ✅ Components view/bills 100% cover

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,21 +1,60 @@
-import { screen } from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
+import { screen } from "@testing-library/dom";
+import BillsUI from "../views/BillsUI.js";
+import { bills } from "../fixtures/bills.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
-      //to-do write expect expression
-    })
+      const html = BillsUI({ data: [] });
+      document.body.innerHTML = html;
+
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+
+      document.body.innerHTML = BillsUI({ data: [] });
+
+      const billIcon = screen.getByTestId("icon-window");
+
+      expect(billIcon).toBeTruthy();
+    });
+
     test("Then bills should be ordered from earliest to latest", () => {
-      const html = BillsUI({ data: bills })
-      document.body.innerHTML = html
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
-  })
-})
+      const html = BillsUI({ data: bills });
+      document.body.innerHTML = html;
+      const dates = screen
+        .getAllByText(
+          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+        )
+        .map((a) => a.innerHTML);
+      const antiChrono = (a, b) => (a < b ? 1 : -1);
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
+  });
+
+  describe("When I am on Bills page but it is loading", () => {
+		test("Then I should land on a loading page", () => {
+			const html = BillsUI({ data: [], loading: true });
+			document.body.innerHTML = html;
+			expect(screen.getAllByText("Loading...")).toBeTruthy();
+		});
+	});
+
+  describe("WHEN i am on Bills page but an error message has been thrown", () => {
+    test("THEN Error page should be rendrered", () => {
+      const html = BillsUI({ error: "some error message" });
+      document.body.innerHTML = html;
+
+      expect(screen.getAllByText("Erreur")).toBeTruthy();
+    });
+  });
+});
